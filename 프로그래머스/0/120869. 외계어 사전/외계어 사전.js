15dd6cc3d131592893541dfc25757e38f0462769
sohee <162614561+do2y@users.noreply.github.com>
2025-03-20 22:58:29 +0900

    [level 0] Title: 외계어 사전, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120869. 외계어 사전/외계어 사전.js
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.js
@@ -2,9 +2,5 @@ function solution(spell, dic) {
     let str = spell.sort().join("")
     
     let sortedDic = dic.map(word => [...word].sort().join(""));
-    if(sortedDic.includes(str)){
-        return 1;
-    }
-    else
-        return 2;
+    return sortedDic.includes(str) ? 1 : 2;
 }
