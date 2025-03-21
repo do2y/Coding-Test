4f39ca15b3cd85389a2c0d51ec643721d140193e
sohee <162614561+do2y@users.noreply.github.com>
2025-03-21 14:06:31 +0900

    [level 1] Title: 2016년, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.js
+++ 프로그래머스/1/12901. 2016년/2016년.js
@@ -1,8 +1,8 @@
 function solution(a, b) {
-    let month = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    let month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
     let days = ["FRI" ,"SAT", "SUN" , "MON" , "TUE" , "WED" , "THU"];
     
-    let sumMonth = month.slice(0,a).reduce((a,c) => a+c, 0);
+    let sumMonth = month.slice(0,a-1).reduce((a,c) => a+c, 0);
     let NumberOfdays = sumMonth+b-1;
     
     return days[NumberOfdays%7];
