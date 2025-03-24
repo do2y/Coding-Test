0cfb52029b19f438c5686247a5a9204495f1bac5
sohee <162614561+do2y@users.noreply.github.com>
2025-03-24 18:58:18 +0900

    [level 1] Title: 소수 찾기, Time: 266.14 ms, Memory: 36.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
@@ -0,0 +1,18 @@
+function solution(n) {
+    let cnt = 0;
+    let isPrime = true;
+    
+    for(let num=3; num<=n; num++) {
+        if(num%2 == 0)   
+                continue;
+        for(let i=3; i<= Math.floor(Math.sqrt(num)); i++) {
+            if(num%i == 0){
+                 isPrime = false;
+                 break;
+            }    
+        }
+        isPrime ? cnt++ : cnt+=0;
+        isPrime = true;
+    }
+    return cnt+1;
+}
