b0a90de60e405b7cd21549baae471de9ca554821
sohee <162614561+do2y@users.noreply.github.com>
2025-04-19 16:53:09 +0900

    [level 2] Title: 올바른 괄호, Time: 4.34 ms, Memory: 37 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,14 @@
+function solution(s){
+    let check = 0;
+    
+    for(let i=0; i<s.length; i++){
+        if(s[i] === "(")
+            check+=1;
+        else if(check >= 1 && s[i] === ")")
+            check-=1;
+        else 
+            return false;
+    }
+    
+    return check == 0 ? true : false;
+}
