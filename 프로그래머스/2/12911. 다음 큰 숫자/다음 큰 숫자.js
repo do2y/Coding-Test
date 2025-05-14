f44959f23b144a77280563dd83cfacfcc1fd519d
sohee <essen030216@naver.com>
2025-05-14 13:40:48 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,15 +1,15 @@
 function solution(n) {
-    let binary = n.toString(2);
-    let cnt_one = [...binary].filter(v => v === '1').length;
+    const countOnes = (num) => num.toString(2).split("1").length - 1;
+    
+    const cnt_one = countOnes(n);
     
     let idx = 1;
     while(true) {
         let next = n+idx;
-        let next_cnt_one = [...next.toString(2)].filter(v => v === '1').length;
-        
-        if(cnt_one === next_cnt_one)
+        if( countOnes(next) === cnt_one )
             return next;
         else
             idx++;
     }
+    
 }
