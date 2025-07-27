dca5c195d8ff30c5fc7aac59c4652e1cba834862
sohee <essen030216@naver.com>
2025-07-27 17:53:30 +0900

    [level 1] Title: 달리기 경주, Time: 213.10 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
@@ -3,19 +3,20 @@ function solution(players, callings) {
     players.forEach((runner, i) => {
         rank[runner] = i;
     });
-
+    
     callings.forEach(call => {
-        let idx = rank[call]; 
+        let idx = rank[call];
         if (idx > 0) {
             let frontRunner = players[idx - 1];
-         
-            players[idx - 1] = call;
+            
+            players[idx -1] = call;
             players[idx] = frontRunner;
-
+            
             rank[call]--;
             rank[frontRunner]++;
         }
     });
-
+    
     return players;
+    
 }
