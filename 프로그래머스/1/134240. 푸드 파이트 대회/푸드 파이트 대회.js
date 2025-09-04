b9216aaef8dfe61d36ed6f414bfcff0e2b43cd8e
sohee <essen030216@naver.com>
2025-09-04 23:31:58 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -0,0 +1,15 @@
+function solution(food) {
+    var answer = '';
+
+    for(let i=1; i<food.length; i++){
+        answer += String(i).repeat(Math.floor(food[i]/2));
+    }
+    
+    answer += "0";
+    
+    for(let i=food.length-1; i>0; i--){
+        answer += String(i).repeat(Math.floor(food[i]/2));
+    }
+    
+    return answer;
+}
