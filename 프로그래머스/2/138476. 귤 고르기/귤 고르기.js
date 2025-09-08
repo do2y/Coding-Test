31782b9ab9dd3392255b4c5bfd4e56bc040a438b
sohee <essen030216@naver.com>
2025-09-08 14:54:12 +0900

    [level 2] Title: 귤 고르기, Time: 190.02 ms, Memory: 81.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -0,0 +1,28 @@
+function solution(k, tangerine) {
+   
+    tangerine.sort((a,b) => a-b);
+    const tangerineSet = new Set(tangerine);
+    
+    const tangerineCount = {};
+    
+    for (let size of tangerine) {
+        if (!tangerineCount[size]) { 
+            tangerineCount[size] = 0; 
+        }
+    
+        tangerineCount[size]++;
+    }
+    
+    let sorted = Object.entries(tangerineCount).sort((a, b) => b[1] - a[1]);
+    
+    let sum = 0;
+    let pickCount = 0;
+    for (let [size, count] of sorted) {
+        sum += count;
+        pickCount++;
+        if ( sum >= k )
+            break;
+    }
+    
+    return pickCount;
+}
