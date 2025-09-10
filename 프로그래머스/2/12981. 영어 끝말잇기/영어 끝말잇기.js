c6930a08610aa8626878bba91c6e81dc763e282b
sohee <essen030216@naver.com>
2025-09-10 18:02:42 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.26 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,22 +1,22 @@
 function solution(n, words) {
     
-    let usedWords = [];
+    let usedWords = new Set();
 
     
     for(let i=0; i<words.length; i++){
         if (i===0) {
-            usedWords.push(words[0]);
+            usedWords.add(words[0]);
             continue;
         }
         
-        if (usedWords.includes(words[i]) || words[i].length === 1
+        if (usedWords.has(words[i]) || words[i].length === 1
             || words[i-1].charAt(words[i-1].length-1) !== words[i].charAt(0)) {
             let player = (i%n) + 1;
             let round = Math.floor(i/n) + 1;
             
             return [player, round];
         }    
-        usedWords.push(words[i]);
+        usedWords.add(words[i]);
     }
     
     return [0, 0];
