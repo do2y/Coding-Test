38557ba41409a93e091ffc2f09bde0c1371cfcf6
sohee <essen030216@naver.com>
2025-09-29 17:42:01 +0900

    [level 2] Title: 할인 행사, Time: 23.22 ms, Memory: 40.1 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -1,42 +1,19 @@
 function solution(want, number, discount) {
     let answer = 0;
     
-    function isSame(buyList, discountList) {
-        if (Object.keys(buyList).length !== Object.keys(discountList).length) return false;
-
-        for (let key in buyList) {
-            if (buyList[key] !== discountList[key]) return false;
+    for (let i=0; i <=discount.length -10; i++) {
+        let tenDays = discount.slice(i, i+10);
+        
+        let flag = 1;
+        for (let j=0; j<want.length; j++) {
+            if (tenDays.filter(item => item === want[j]).length !== number[j]) {
+                flag = 0;
+                break;
+            }   
         }
-        return true;
-    }
-    
-    
-    let mustBuy = {};
-    for (let i=0; i<want.length; i++) {
-        mustBuy[want[i]] = number[i];
-    }
-    
-    let tenDays = {};
-    for (let i=0; i<10; i++) {
-        let item = discount[i];
-        tenDays[item] = (tenDays[item] || 0) + 1;
-    }
-    if (isSame(mustBuy, tenDays)) answer++;
-     
-    for (let i=10; i<discount.length; i++) {
-        let outItem = discount[i-10];
-        let inItem = discount[i];
         
-        tenDays[outItem]--;
-        if (tenDays[outItem] === 0) delete tenDays[outItem];
-        
-        tenDays[inItem] = (tenDays[inItem] || 0) + 1;
-        
-        //비교
-        if (isSame(mustBuy,tenDays))
-            answer++;
+        if (flag) answer++;
     }
     
     return answer;
-
 }
