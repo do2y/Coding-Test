4c1c66178398b7dba135d2c364648000f26cc812
sohee <essen030216@naver.com>
2025-09-29 19:03:35 +0900

    [level 2] Title: 예상 대진표, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -1,22 +1,11 @@
-function solution(n,a,b)
-{
-    let round = 0; 
-    
-    while (n >= 2) {
-        round++;
+function solution(n, a, b) {
+    let round = 0;
 
-        let min = Math.min(a,b);
-        let max = Math.max(a,b);
-        
-        if ((max - min === 1) && (min % 2 === 1))
-            break;
-        
-        a = Math.ceil(a/2);
-        b = Math.ceil(b/2);
-        
-        n/=2;
-       
+    while (a !== b) {
+        a = Math.ceil(a / 2);
+        b = Math.ceil(b / 2);
+        round++;
     }
-  
+
     return round;
 }
