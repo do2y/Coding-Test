ff015a9c4e651cc87fc139538ef0d83b8a363883
sohee <essen030216@naver.com>
2025-09-30 13:42:32 +0900

    [level 2] Title: 카펫, Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -1,11 +1,5 @@
 function solution(brown, yellow) {
-    
-    /* 
-    1. 가로 * 세로 = yellow를 만족하고 
-      (2*가로) + (2*세로) + 4 = brown을 만족하는 가로 세로 찾기
-      -> 그런데 이때 가로 >= 세로  
-    */
-    
+
     let width = 1;
     let height = 1;
     
