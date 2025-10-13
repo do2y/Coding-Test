595ac3819ad6f555fee43cdc88e93eef8e8b223a
sohee <essen030216@naver.com>
2025-10-13 14:15:31 +0900

    [level 2] Title: 의상, Time: 0.24 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,13 +1,14 @@
 function solution(clothes) {
+    
     let categoryCount = {};
     
     clothes.forEach(([_, cloth]) => {
-        categoryCount[cloth] = (categoryCount[cloth] || 0) + 1;
+       categoryCount[cloth] = (categoryCount[cloth] || 0) + 1;
     });
-
+    
     let answer = 1;
     for (let key in categoryCount) {
-        answer *= categoryCount[key] + 1;
+     answer *= categoryCount[key] + 1;
     }
     
     return answer - 1;
