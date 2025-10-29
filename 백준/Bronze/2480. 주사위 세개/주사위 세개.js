862104bd885414b9c66e8cc00dc5aae25e993c38
sohee <essen030216@naver.com>
2025-10-29 22:34:42 +0900

    [Bronze IV] Title: 주사위 세개, Time: 92 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+let input = fs
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "input.txt")
+  .toString()
+  .trim()
+  .split(" ")
+  .map(Number);
+
+let diceMap = {};
+
+input.forEach((num) => {
+  diceMap[num] = (diceMap[num] || 0) + 1;
+});
+
+let sameDice = Object.keys(diceMap).find((k) => diceMap[k] >= 2);
+const values = Object.values(diceMap);
+let prize = 0;
+
+if (values.includes(2)) prize = 1000 + sameDice * 100;
+else if (values.includes(3)) prize = 10000 + sameDice * 1000;
+else prize = Math.max(...input) * 100;
+
+console.log(prize);
