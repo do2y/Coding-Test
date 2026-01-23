cd280733106097cf8d0967e124ff6b22031f2705
sohee <essen030216@naver.com>
2026-01-23 19:28:32 +0900

    [Gold V] Title: AC, Time: 272 ms, Memory: 36712 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.js
+++ 백준/Gold/5430. AC/AC.js
@@ -3,6 +3,7 @@ const input = fs
   .readFileSync(process.platform === "linux" ? "/dev/stdin" : "input.txt")
   .toString()
   .trim()
+  .replace(/\r/g, "")
   .split("\n");
 
 const testCaseCount = input[0];
@@ -11,27 +12,32 @@ let answer = [];
 
 for (let i = 0; i < testCaseCount; i++) {
   const cmd = testCases[3 * i];
-  const cnt = testCases[3 * i + 1];
   let str = testCases[3 * i + 2];
   const arr = str.length === 2 ? [] : str.slice(1, -1).split(",").map(Number);
 
   let flag = 0;
-  let reverse = 0;
+  let isReverse = 0;
+
+  let head = 0;
+  let tail = arr.length - 1;
+
   for (let i = 0; i < cmd.length; i++) {
     if (cmd[i] === "R") {
-      reverse = !reverse;
+      isReverse = !isReverse;
     } else {
-      if (arr.length >= 1) reverse ? arr.pop() : arr.shift();
-      else {
+      if (head > tail) {
         answer.push("error");
         flag = 1;
         break;
       }
+      isReverse ? tail-- : head++;
     }
   }
+
   if (!flag) {
-    if (reverse) arr.reverse();
-    answer.push("[" + arr.toString() + "]");
+    let slicedArr = arr.slice(head, tail + 1);
+    if (isReverse) slicedArr.reverse();
+    answer.push("[" + slicedArr.join(",") + "]");
   }
 }
 
