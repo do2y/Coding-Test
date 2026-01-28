711cc035b396add6418455f65dd591a2c84aa993
sohee <essen030216@naver.com>
2026-01-29 01:34:58 +0900

    [Silver III] Title: 프린터 큐, Time: 88 ms, Memory: 9660 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.js
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.js
@@ -1,39 +1,35 @@
 const fs = require("fs");
-let input = fs
-  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "input.txt")
-  .toString()
-  .trim()
-  .split("\n");
+const input = fs.readFileSync(0, "utf8").trim().split("\n");
 
 const T = Number(input[0]);
-const testCases = input.slice(1);
+const tc = input.slice(1);
+
 let answer = [];
 
 let idx = 0;
 for (let i = 0; i < T; i++) {
-  const [N, M] = testCases[idx++].split(" ").map(Number);
-  const priorities = testCases[idx++].split(" ").map(Number);
+  const [N, M] = tc[idx++].split(" ").map(Number);
+  const priorities = tc[idx++].split(" ").map(Number);
 
   let queue = [];
   for (let j = 0; j < N; j++) {
     queue.push({ idx: j, priority: priorities[j] });
   }
 
-  const priorityList = priorities.sort((a, b) => b - a);
+  let priorityList = priorities.sort((a, b) => b - a);
+
   let cnt = 0;
   while (true) {
-    const first = queue.shift();
-
-    if (first.priority === priorityList[0]) {
+    const front = queue.shift();
+    if (front.priority === priorityList[0]) {
       cnt++;
       priorityList.shift();
 
-      if (first.idx === M) {
-        answer.push(cnt);
-        break;
-      }
-    } else queue.push(first);
+      if (front.idx === M) break;
+    } else queue.push(front);
   }
+
+  answer.push(cnt);
 }
 
 console.log(answer.join("\n"));
