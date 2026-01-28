90d2d655c547dd9d0b42c3f0c60286e1ef5200d2
sohee <essen030216@naver.com>
2026-01-28 20:39:05 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 92 ms, Memory: 9380 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const input = fs.readFileSync(0, "utf8").trim();
+
+const n = Number(input);
+const arr = [];
+
+for (let i = 1; i <= n; i++) {
+  const str = " ".repeat(n - i) + "*".repeat(i);
+  arr.push(str);
+}
+
+console.log(arr.join("\n"));
