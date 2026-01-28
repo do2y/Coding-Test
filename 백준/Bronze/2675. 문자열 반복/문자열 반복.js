7903f42be9511b2e2a113dccc9ba3abc8438de33
sohee <essen030216@naver.com>
2026-01-28 20:52:28 +0900

    [Bronze II] Title: 문자열 반복, Time: 88 ms, Memory: 9324 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.js
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.js
@@ -2,14 +2,17 @@ const fs = require("fs");
 const input = fs.readFileSync(0, "utf8").trim().split("\n");
 
 const T = Number(input[0]);
-const testCases = input.slice(1);
-
 const answer = [];
-for (let i = 0; i < T; i++) {
-  let [times, str] = testCases[i].split(" ");
 
-  str = str.split("").map((char) => char.repeat(times));
-  answer.push(str.join(""));
+for (let i = 1; i <= T; i++) {
+  const [timesStr, word] = input[i].split(" ");
+  const times = Number(timesStr);
+
+  let converted = "";
+  for (const ch of word) {
+    converted += ch.repeat(times);
+  }
+  answer.push(converted);
 }
 
 console.log(answer.join("\n"));
