b14e34b55d730f8f0969fad42b3e06c2b0207dce
sohee <essen030216@naver.com>
2026-03-19 14:49:26 +0900

    [Silver II] Title: 에디터, Time: 468 ms, Memory: 102932 KB -BaekjoonHub

---
+++ 백준/Silver/1406. 에디터/에디터.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync(0, "utf8")
+  .trim()
+  .replaceAll(/\r/g, "")
+  .split("\n");
+
+const str = input[0];
+const cmdCnt = +input[1];
+const cmds = input.slice(2);
+
+const left = str.split("");
+const right = [];
+
+for (let i = 0; i < cmdCnt; i++) {
+  let cmd = cmds[i];
+
+  if (cmd === "L") {
+    let popped = left.pop();
+    if (popped) {
+      right.push(popped);
+    }
+  } else if (cmd === "D") {
+    if (right.length) {
+      left.push(right.pop());
+    }
+  } else if (cmd === "B") {
+    if (left.length) {
+      left.pop();
+    }
+  } else {
+    const [_, alpha] = cmd.split(" ");
+    left.push(alpha);
+  }
+}
+
+let answer = left.join("") + right.reverse().join("");
+console.log(answer);
