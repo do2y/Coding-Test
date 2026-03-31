55002257e54a3b35fa1ccd2b1f7bd323eb11a67a
sohee <essen030216@naver.com>
2026-03-31 14:47:24 +0900

    [Silver IV] Title: 기타줄, Time: 96 ms, Memory: 9348 KB -BaekjoonHub

--- 백준/Silver/1049. 기타줄/기타줄.js
+++ 백준/Silver/1049. 기타줄/기타줄.js
@@ -2,22 +2,15 @@ const fs = require("fs");
 const input = fs.readFileSync(0, "utf8").trim().split("\n");
 
 const [N] = input[0].split(" ").map(Number);
-
 const prices = input.slice(1).map((v) => v.split(" ").map(Number));
 
 const minPackagePrice = Math.min(...prices.map((v) => v[0]));
 const minUnitPrice = Math.min(...prices.map((v) => v[1]));
 
-let total = 0;
-if (minUnitPrice < minPackagePrice / 6) {
-  total = minUnitPrice * N;
-} else {
-  total = Math.floor(N / 6) * minPackagePrice;
-
-  let res = N % 6;
-  if (minUnitPrice * res < minPackagePrice) {
-    total += minUnitPrice * res;
-  } else total += minPackagePrice;
-}
+const total = Math.min(
+  minPackagePrice * Math.ceil(N / 6),
+  minPackagePrice * Math.floor(N / 6) + minUnitPrice * (N % 6),
+  minUnitPrice * N,
+);
 
 console.log(total);
