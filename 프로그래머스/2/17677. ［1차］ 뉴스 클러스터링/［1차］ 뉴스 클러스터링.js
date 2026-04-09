3888b677cd8e388509a6dac2949a72aa8c087bcf
sohee <essen030216@naver.com>
2026-04-09 10:33:19 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 2.10 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
@@ -1,72 +1,64 @@
+const ENG_REGEX = /^[a-zA-Z]{2}$/;
+
 function solution(str1, str2) {
-    let answer = 0;
-    let jaccardSimilarity = 1;
+    const [set1, set2] = makeSet(str1, str2);
+    const [freq1, freq2] = countWord(set1, set2);
     
-    let [set1, set2] = makeSet(str1, str2);
+    const [intersectCount, unionCount] = countSet(freq1, freq2);
     
-    if (set1.length !== 0 || set2.length !== 0) {
-        let [set1CountDict, set2CountDict] = countWord(set1, set2);
-        let [intersectCount, unionCount] = countSet(set1CountDict, set2CountDict);
-        jaccardSimilarity = intersectCount / unionCount;
-    }
+    if (unionCount === 0) return 65536;
     
-    return Math.trunc(jaccardSimilarity * 65536);
+    return Math.trunc((intersectCount / unionCount) * 65536);
 }
 
 function makeSet(str1, str2) {
     const set1 = [];
     const set2 = [];
     
-    let eng = /^[a-zA-Z]{2}$/;
-    let ch;
-    
-    for (let i=0; i<str1.length - 1; i++) {
-        ch = str1[i] + str1[i+1];
-        if (eng.test(ch))
-            set1.push(ch.toUpperCase());
+    for (let i = 0; i < str1.length - 1; i++) {
+        const ch = str1[i] + str1[i + 1];
+        if (ENG_REGEX.test(ch)) set1.push(ch.toUpperCase());
     }
     
-    for (let i=0; i<str2.length - 1; i++) {
-        ch = str2[i] + str2[i+1];
-        if (eng.test(ch))
-            set2.push(ch.toUpperCase());
+    for (let i = 0; i < str2.length - 1; i++) {
+        const ch = str2[i] + str2[i + 1];
+        if (ENG_REGEX.test(ch)) set2.push(ch.toUpperCase());
     }
+    
     return [set1, set2];
 }
 
 function countWord(set1, set2) {
-    const set1CountDict = {};
-    const set2CountDict = {};
+    const freq1 = {};
+    const freq2 = {};
+    
     set1.forEach(ch => {
-        set1CountDict[ch] = (set1CountDict[ch] || 0) + 1; 
+        freq1[ch] = (freq1[ch] || 0) + 1;
     });
     
     set2.forEach(ch => {
-        set2CountDict[ch] = (set2CountDict[ch] || 0) + 1; 
+        freq2[ch] = (freq2[ch] || 0) + 1;
     });
     
-    return [set1CountDict, set2CountDict];
+    return [freq1, freq2];
 }
 
-function countSet(countDict1, countDict2) {
+function countSet(freq1, freq2) {
     let intersectCount = 0;
     let unionCount = 0;
     
-    for (let key in countDict1) {
-        if (countDict2[key]) {
-            intersectCount += Math.min(countDict1[key], countDict2[key]);
-            unionCount += Math.max(countDict1[key], countDict2[key]);
-        } else {
-            unionCount += countDict1[key];
-        }
-    }
-    for (let key in countDict2) {
-        if (!countDict1[key]) unionCount += countDict2[key];
+    const keys = new Set([
+        ...Object.keys(freq1),
+        ...Object.keys(freq2)
+    ]);
+    
+    for (let key of keys) {
+        const a = freq1[key] || 0;
+        const b = freq2[key] || 0;
+        
+        intersectCount += Math.min(a, b);
+        unionCount += Math.max(a, b);
     }
     
     return [intersectCount, unionCount];
 }
-
-
-
-
